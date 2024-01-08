b6f35654c90f2088d3df0c174942b9600e3e913a
philippstrunk <philipp.strunk@icloud.com>
2024-01-08 22:44:12 +0100

    Abgabe der Aufgabe 8

---
+++ src/org/hbrs/se1/ws23/uebung8/Travel/ExternalTravelProvider.java
@@ -0,0 +1,15 @@
+package org.hbrs.se1.ws23.uebung8.Travel;
+
+/**
+ * @author pstrun2s
+ */
+
+public class ExternalTravelProvider {
+
+    public ExternalTravelInfo getTravelInformation(String destination) {
+        // Code, um Reiseinformationen vom externen Anbieter zu erhalten
+        return new ExternalTravelInfo();
+
+    }
+
+}
